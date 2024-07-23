d7bcd827be563ccbc871417388162875af10c128
CrazyDream000 <danakimoto319@gmail.com>
2024-07-22 23:27:04 -0400

    updated

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -60,53 +60,9 @@ contract Staking is ReentrancyGuard {
         s_rewardToken = IERC20(rewardToken);
     }
 
-    function earned(address account) public view returns (uint256) {
-        uint256 currentBalance = s_balances[account];
-        // how much they were paid already
-        uint256 amountPaid = s_userRewardPerTokenPaid[account];
-        uint256 currentRewardPerToken = rewardPerToken();
-        uint256 pastRewards = s_rewards[account];
-        uint256 _earned = ((currentBalance * (currentRewardPerToken - amountPaid)) / 1e18) +
-            pastRewards;
 
-        return _earned;
-    }
 
-    /** @dev Basis of how long it's been during the most recent snapshot/block */
-    function rewardPerToken() public view returns (uint256) {
-        if (s_totalSupply == 0) {
-            return s_rewardPerTokenStored;
-        } else {
-            return
-                s_rewardPerTokenStored +
-                (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalSupply);
-        }
-    }
 
-    function stake(uint256 amount) external updateReward(msg.sender) moreThanZero(amount) {
-        // keep track of how much this user has staked
-        // keep track of how much token we have total
-        // transfer the tokens to this contract
-        /** @notice Be mindful of reentrancy attack here */
-        s_balances[msg.sender] += amount;
-        s_totalSupply += amount;
-        //emit event
-        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
-        // require(success, "Failed"); Save gas fees here
-        if (!success) {
-            revert Staking__TransferFailed();
-        }
-    }
-
-    function withdraw(uint256 amount) external updateReward(msg.sender) moreThanZero(amount) {
-        s_balances[msg.sender] -= amount;
-        s_totalSupply -= amount;
-        // emit event
-        bool success = s_stakingToken.transfer(msg.sender, amount);
-        if (!success) {
-            revert Withdraw__TransferFailed();
-        }
-    }
 
     function claimReward() external updateReward(msg.sender) {
         uint256 reward = s_rewards[msg.sender];
